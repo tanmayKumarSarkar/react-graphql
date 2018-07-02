6774a69391d2912a69720962c477f2a8439ca668
tanmaykumar1993 <tanmay.kr.sarkar@live.com>
2018-07-02 09:28:31 +0530

    update

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,7 @@ const client = new ApolloClient({
 class App extends Component {
   render() {
     return (
-      <ApolloProvider client={client}>bb
+      <ApolloProvider client={client}>
          <div className="App" id="main">
         <h1>Reading List</h1>
         <BookList />
